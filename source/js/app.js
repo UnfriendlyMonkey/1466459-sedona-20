ca05f6e861324ca547949f1103bab722c3252844
Andrey Sereda <andreysereda@ya.ru>
2020-09-14 20:08:30 +0300

    правки по замечаниям проверяющего наставника

--- source/js/app.js
+++ source/js/app.js
@@ -2,19 +2,11 @@ var mainNav = document.querySelector(".main-nav");
 var navClose = document.querySelector(".main-nav__close")
 var openNav = document.querySelector(".page-header__open-menu")
 
-// var isStorageSupport = true;
-// var storage = "";
-
-// try {
-//   storage = localStorage.getItem("adults");
-// } catch (err) {
-//   isStorageSupport = false;
-// }
-
 window.onload = function() {
   if (mainNav.classList.contains("main-nav--appear")) {
       mainNav.classList.remove("main-nav--appear");
       openNav.classList.remove("visually-hidden");
+      navClose.classList.remove("visually-hidden");
   }
 };
 
